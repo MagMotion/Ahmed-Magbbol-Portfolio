2d800c1a324b2e1d089745f9a33fc38d6c39d427
Ahmed Magbool <ahmad209a@gmail.com>
2024-08-24 01:36:36 -0400

    change the logo and the favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 
+import magboolLogo from "../assets/MagboolLogo.png";
 import { FaLinkedin } from "react-icons/fa"; 
 import { FaVimeo } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6"; 
@@ -11,7 +12,7 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center magboolLogo">
-            <h2 className="font">Magbool</h2>
+            <img className="w-8" src={magboolLogo} alt="" />
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
             <a href="https://www.linkedin.com/in/ahmadmagbool/" target="_blank"><FaLinkedin/></a>
